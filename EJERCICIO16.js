20cd9b603872a832d522a1a09864ecada9860fbd
jeaansalazar <118024330+jeaansalazar@users.noreply.github.com>
2022-11-17 18:16:00 -0500

    se agrega el proyecto

---
+++ EJERCICIO16.js
@@ -0,0 +1,51 @@
+
+// let multipli = function(x,y){
+//   return x * y
+// }
+
+// function expon (x,y) {
+//   return Math.pow(x,y)
+// }
+
+// let suma = function (x,y) {
+//     return x + y
+// }
+
+// let resta = function(x,y) {
+//     return x - y
+// }
+
+// var result = multipli(5,2)
+// alert(result)
+// result = suma(3,4)
+// alert(result)
+// result=resta(9,7)
+// alert(result)
+
+
+
+
+
+
+// let pasarFarenheit = (grado)=> {
+//     return ( farenheit= grado * 1.8 + 32)
+// }
+// var result= pasarFarenheit(150)
+// alert(result)
+
+// let pasarCelsius = (grado)=> {
+//     return (Celsius = (grado - 32) * (5/9))
+// }
+// var result= pasarCelsius(150)
+// alert(result.toFixed(2))
+
+
+
+
+// let a =+prompt("Ingresar primer numero")
+// let b =+prompt("Ingresar otro numero")
+// function suma(a,b){
+//     return a + b
+// }
+// alert(`La suma de ${a} + ${b} es ${suma(a,b)}`)
+
